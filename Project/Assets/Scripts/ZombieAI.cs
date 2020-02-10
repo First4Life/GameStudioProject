7a0d4bf37304b70ab84b7a207d028dda983a2cb3
Samuel Brown <samuelbrown3199@gmail.com>
2020-02-10 14:45:44 +0000

    Zombie Pathfinding and Early State machine

--- Project/Assets/Scripts/ZombieAI.cs
+++ Project/Assets/Scripts/ZombieAI.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieAI : MonoBehaviour
 {
+    public Transform playerPos;
+    NavMeshAgent pathFinding;
+
+    float distanceToPlayer;
+
     void Start()
     {
-        
+        pathFinding = GetComponent<NavMeshAgent>();
     }
-
+    
     void Update()
     {
-        UpdatePath();
+        distanceToPlayer = Vector3.Distance(this.transform.position, playerPos.position);
+
+        if (distanceToPlayer > 2f)
+        {
+            UpdatePath();
+        }
     }
 
     void UpdatePath()
     {
-        //iupagndjfsan
+        Vector3 differencePosition = this.transform.position - playerPos.position;
+        Vector3 targetDirection = differencePosition.normalized;
+        Vector3 targetPosition = playerPos.position + (targetDirection * 2f);
+
+        pathFinding.SetDestination(targetPosition);
     }
 }
