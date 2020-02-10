ff4f9d16a8ddb5fd94f91de55d8615d32d9fa5b5
DavidWong85 <50549827+DavidWong85@users.noreply.github.com>
2020-02-10 11:29:54 +0000

    Mouse & Movement

---
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 8f;
+    public float gravity = -5f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGerounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, )
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
