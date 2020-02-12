f7b7bca2d392a4a68e599950b4fd0d1f0b3cd98a
MechSam <samuelbrown3199@gmail.com>
2020-02-12 11:18:44 +0000

    AI tests and minor fix

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -39,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Squrt(jumpHeight * -2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
--- Project/Assets/Scripts/ZombieAI.cs
+++ Project/Assets/Scripts/ZombieAI.cs
@@ -9,6 +9,7 @@ public class ZombieAI : MonoBehaviour
     NavMeshAgent pathFinding;
 
     float distanceToPlayer;
+    Vector3 oldPlayerPos;
 
     void Start()
     {
@@ -27,10 +28,16 @@ public class ZombieAI : MonoBehaviour
 
     void UpdatePath()
     {
-        Vector3 differencePosition = this.transform.position - playerPos.position;
-        Vector3 targetDirection = differencePosition.normalized;
-        Vector3 targetPosition = playerPos.position + (targetDirection * 2f);
+        if (playerPos.position != oldPlayerPos)
+        {
+            Vector3 differencePosition = this.transform.position - playerPos.position;
+            Vector3 targetDirection = differencePosition.normalized;
+            Vector3 targetPosition = playerPos.position + (targetDirection * 2f);
 
-        pathFinding.SetDestination(targetPosition);
+            pathFinding.SetDestination(targetPosition);
+            oldPlayerPos = playerPos.position;
+
+            Debug.Log("Created new path to player");
+        }
     }
 }
