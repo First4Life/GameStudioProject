9836fd6364bea5a637c28044595ec9edb1539647
Tsz Wong (s5084458) <s5084458@bournemouth.ac.uk>
2020-02-19 12:05:23 +0000

    Door ( 90 Degree )

---
+++ Project/Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float Angle = 90.0f;
+    public float openSpeed = 2.0f;
+
+    public bool open = false;
+
+    float defaultRoatationAngle;
+    float currentRotationAngle;
+    float openTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultRoatationAngle = transform.localEulerAngles.y;
+        currentRotationAngle = transform.localEulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (openTime < 1)
+        {
+            openTime += Time.deltaTime * openSpeed;
+        }
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Mathf.LerpAngle(currentRotationAngle, defaultRoatationAngle + (open ? Angle : 0), openTime), transform.localEulerAngles.z);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            open = !open;
+            currentRotationAngle = transform.localEulerAngles.y;
+            openTime = 0;
+        }
+    }
+}
